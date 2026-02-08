7af0d1aa73985544f741765535e59caeb31404c7
Joao Ferreira <joaobizuca10@gmail.com>
2026-02-08 15:34:03 -0300

    fix: setting pm10 nullable and not required

--- AirAware/ViewModels/CreateReadingViewModel.cs
+++ AirAware/ViewModels/CreateReadingViewModel.cs
@@ -8,7 +8,6 @@ public class CreateReadingViewModel
     public Guid StationId { get; set; }
     [Required]
     public double Pm25 { get; set; }
-    [Required]
-    public double Pm10 { get; set; }
+    public double? Pm10 { get; set; }
     public string? RawPayload { get; set; }
 }
